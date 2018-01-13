df669b55308d53f877334e2dfe91ce1104e53b63
luapvu <luapvu@gmail.com>
2018-01-13 15:50:37 -0800

    include keys.js

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientId: "184022079079-kp7i8s81qb3lm2dog7rp0sahj2gorbf3.apps.googleusercontent.com",
+    googleClientSecret: "t02-FArOj3qRwhrvkiMjrVWz",
+    twitterConsumerKey:	"vIT8ChzmZKNHjtqFOMN9Yz2kt",
+    twitterConsumerSecret: "pCBxpbM50lQE6MAEjYoQEpr8QBiCAgYdkh9q5IqaYZuzJkuIH2",
+    mongoURI: 'mongodb://dennisjs0910:Admin123Admin123@ds251217.mlab.com:51217/nwhack2018'
+}
