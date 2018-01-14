a32310ae6bae96a75b96975b5d1cef8042e7716f
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2018-01-13 16:25:01 -0800

    minor changes

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleClientId: "184022079079-kp7i8s81qb3lm2dog7rp0sahj2gorbf3.apps.googleusercontent.com",
+    googleClientSecret: "t02-FArOj3qRwhrvkiMjrVWz",
+    mongoURI: 'mongodb://dennisjs0910:Admin123Admin123@ds251217.mlab.com:51217/nwhack2018'
+}
