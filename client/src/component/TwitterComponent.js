04d3615388adb97152f5cc054bcb409657dd7c5f
annie001 <anniewong_197@hotmail.com>
2018-01-13 17:20:22 -0800

    add stuff

--- client/src/component/TwitterComponent.js
+++ client/src/component/TwitterComponent.js
@@ -4,8 +4,8 @@ import './index.css';
 class TwitterComponent extends React.Component {
     render() {
         return (
-            <div className="panel panel-primary" width="10px">
-                <div class="panel-heading" width="10px">Panel Heading</div>
+            <div className="panel panel-primary">
+                <div class="panel-heading">Panel Heading</div>
                 <div class="panel-body">Panel Content</div>
             </div>
         );
