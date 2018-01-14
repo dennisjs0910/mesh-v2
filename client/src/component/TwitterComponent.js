ccc90f18782e6f9819957904e8df3830093d8734
annie001 <anniewong_197@hotmail.com>
2018-01-13 17:15:30 -0800

    added index.css and updates to TwitterComponents

---
+++ client/src/component/TwitterComponent.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './index.css';
+
+class TwitterComponent extends React.Component {
+    render() {
+        return (
+            <div className="panel panel-primary" width="10px">
+                <div class="panel-heading" width="10px">Panel Heading</div>
+                <div class="panel-body">Panel Content</div>
+            </div>
+        );
+    }
+}
+
+export default TwitterComponent;
