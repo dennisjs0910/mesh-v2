f6102fccc02df515312823d36e28170afc381d60
Rostradamus <hyungro@hotmail.com>
2018-02-04 23:21:51 -0800

    installed redux-logger in the module

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+import logger from 'redux-logger';
 import App from './component/App';
 import reducers from './reducers';
 
@@ -10,7 +11,7 @@ import axios from 'axios';
 window.axios = axios;
 
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk, logger));
 store.subscribe(() => console.log(store.getState()));
 
 ReactDOM.render(
